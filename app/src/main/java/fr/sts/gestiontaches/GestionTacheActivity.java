4d0ac6742f09a1c3ed0db247994bb94875f1e309
ftrudell <ftrudell@LMOWKS600021>
2020-11-16 13:55:43 +0100

    Set double tap event

--- app/src/main/java/fr/sts/gestiontaches/GestionTacheActivity.java
+++ app/src/main/java/fr/sts/gestiontaches/GestionTacheActivity.java
@@ -2,14 +2,65 @@ package fr.sts.gestiontaches;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class GestionTacheActivity extends AppCompatActivity {
+
+    EditText etNomTache;
+    DatePicker dpDateTache;
+    int iPositionTache;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gestion_tache);
-        
+
+        etNomTache = (EditText) findViewById(R.id.etNomTache);
+        dpDateTache = (DatePicker) findViewById(R.id.dpDate);
+
+        String _sNomTache = getIntent().getStringExtra("nomTache");
+        String _sDateTache = getIntent().getStringExtra("dateTache");
+        iPositionTache = getIntent().getIntExtra("position", 0);
+
+        if (_sNomTache != "" && _sDateTache != null) {
+            String[] _arrDateTache = _sDateTache.trim().split("/");
+            etNomTache.setText(_sNomTache);
+            try {
+                //Calendar month is 0 based
+                dpDateTache.updateDate(Integer.parseInt(_arrDateTache[2]), Integer.parseInt(_arrDateTache[1]) - 1, Integer.parseInt(_arrDateTache[0]));
+            } catch (Exception e) {
+                String _sErrorMessage = e.getMessage();
+            }
+        }
+    }
+
+    public void CMTache(View view) {
+        etNomTache = (EditText) findViewById(R.id.etNomTache);
+        dpDateTache = (DatePicker) findViewById(R.id.dpDate);
+        String dateString = dpDateTache.getDayOfMonth() + "/" + (dpDateTache.getMonth() + 1) + "/" + dpDateTache.getYear();
+
+
+        Intent i = new Intent();
+        i.putExtra("nomTache", etNomTache.getText().toString());
+        i.putExtra("dateTache", dateString);
+        i.putExtra("position", iPositionTache);
+        setResult(this.RESULT_OK, i);
+        finish();
+    }
+
+    public void AnnulerTache(View view) {
+        Intent i = new Intent();
+        setResult(this.RESULT_CANCELED, i);
+        finish();
     }
 }
--- app/src/main/java/fr/sts/gestiontaches/MainActivity.java
+++ app/src/main/java/fr/sts/gestiontaches/MainActivity.java
@@ -4,15 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewConfiguration;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int REQUEST_CODE = 1;
+    int REQUEST_CODE_AJOUT = 1;
+    int REQUEST_CODE_MODIF = 2;
     ArrayAdapter<String> lvAdaptateur;
     ListView lvTaches;
+    long lastClickTime;
+    int lastPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,23 +28,58 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         this.setTitle("Mes tâches");
-        lvTaches = (ListView)findViewById(R.id.lvTaches);
-        
+        lvTaches = (ListView) findViewById(R.id.lvTaches);
+
         // L'adaptateur adapte la vue (layout) et le modèle des éléments
         // Le layout pour visualiser une chaîne existe nativement dans Android
         // simple_list_item_1
-        lvAdaptateur =  new ArrayAdapter<String>(this ,
+        lvAdaptateur = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1);
-        lvTaches.setAdapter( lvAdaptateur );
+        lvTaches.setAdapter(lvAdaptateur);
 
         lvAdaptateur.add("Comprendre les Intents -> 16/11/2020");
         lvAdaptateur.add("Comprendre les ListViews -> 16/11/2020");
         lvAdaptateur.add("Faire le TP android -> 23/11/2020");
+        
+        lvTaches.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                long currTime = System.currentTimeMillis();
+                int currPosition = position;
+                if (currTime - lastClickTime <= ViewConfiguration.getDoubleTapTimeout() && currPosition == lastPosition) {
+                    onItemDoubleClick(adapterView, view, position, l);
+                }
+                lastClickTime = currTime;
+                lastPosition = currPosition;
+            }
 
+            public void onItemDoubleClick(AdapterView<?> adapterView, View view, int position, long l) {
+                String[] _arrTache = adapterView.getItemAtPosition(position).toString().trim().split("->");
+
+                Intent i = new Intent(getBaseContext(), GestionTacheActivity.class);
+                i.putExtra("nomTache", _arrTache[0]);
+                i.putExtra("dateTache", _arrTache[1]);
+                i.putExtra("position", position);
+                startActivityForResult(i, REQUEST_CODE_MODIF);
+            }
+        });
     }
 
     public void AjoutTache(View view) {
         Intent i = new Intent(this, GestionTacheActivity.class);
-        startActivityForResult(i, REQUEST_CODE);
+        startActivityForResult(i, REQUEST_CODE_AJOUT);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE_AJOUT) {
+            lvAdaptateur.add(data.getStringExtra("nomTache") + " -> " + data.getStringExtra("dateTache"));
+        } else if (resultCode == RESULT_OK && requestCode == REQUEST_CODE_MODIF) {
+            String _tTache = lvAdaptateur.getItem(data.getIntExtra("position", 0));
+            lvAdaptateur.remove(_tTache);
+            _tTache = data.getStringExtra("nomTache") + " -> " + data.getStringExtra("dateTache");
+            lvAdaptateur.add(_tTache);
+            lvAdaptateur.notifyDataSetChanged();
+        }
     }
 }
